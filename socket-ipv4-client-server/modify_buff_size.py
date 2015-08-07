03bc220ce93c6dd77ea72ef304a53e888585fd2d
PHPGeek Programmer <mujib.programmer@gmail.com>
2015-08-07 16:28:26 +0700

    add comment to modify_buff_size.py file

--- socket-ipv4-client-server/modify_buff_size.py
+++ socket-ipv4-client-server/modify_buff_size.py
@@ -5,6 +5,12 @@
 
 import socket
 
+"""
+First, define two constants: SEND_BUF_SIZE / RECV_BUF_SIZE and then wrap a socket
+instance's call to the setsockopt() method in a function. It is also a good idea to check the
+value of the buffer size before modifying it. Note that we need to set up the send and receive
+buffer size separately.
+"""
 SEND_BUF_SIZE = 4096
 RECV_BUF_SIZE = 4096
 
@@ -30,5 +36,12 @@ def modify_buff_size():
     bufsize = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
     print "Buffer size [After]:%d" %bufsize
 
+"""
+If you run the preceding script, it will show the changes in the socket's buffer size. The following
+output may be different on your machine depending on your operating system's local settings:
+$ python modify_buff_size.py
+Buffer size [Before]:16384
+Buffer size [After]:8192
+"""
 if __name__ == '__main__':
     modify_buff_size()
